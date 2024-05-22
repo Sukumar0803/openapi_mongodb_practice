530116483af3f0fbf01bbaf9c958028a4a299d39
Sukumar0803 <kalpam.sukumar@gmail.com>
2024-05-23 02:04:45 +0530

    Initial changes

---
+++ src/main/java/com/practice/mongodb/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.practice.mongodb.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/test", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+public class TestController {
+
+    @GetMapping("/message")
+    public ResponseEntity<String> sayHello() {
+        return new ResponseEntity<>("hellow world", HttpStatus.OK);
+    }
+}
