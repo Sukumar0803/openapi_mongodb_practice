188e7e5a4f4987389ede6ed333eef85ad1525692
Sukumar0803 <kalpam.sukumar@gmail.com>
2024-05-25 00:04:46 +0530

    HATEOUS and Actuator changes

--- src/main/java/com/practice/mongodb/controller/BookController.java
+++ src/main/java/com/practice/mongodb/controller/BookController.java
@@ -4,11 +4,16 @@ import com.practice.mongodb.dto.BookDTO;
 import com.practice.mongodb.entity.Book;
 import com.practice.mongodb.mapper.BookMapper;
 import com.practice.mongodb.services.BookService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/api/v1/books")
@@ -33,14 +38,20 @@ public class BookController {
         return new ResponseEntity<>(bookService.createBook(book), HttpStatus.CREATED);
     }
 
+    /**
+     * Just For Hateous Learning
+     */
     @GetMapping("/filters")
-    public ResponseEntity<?> getBooksBasedOnFilters(@RequestParam(required = false) Integer id, @RequestParam(required = false) String name,
-                                                    @RequestParam(required = false) String author,
-                                                    @RequestParam(required = false) Integer cost, @RequestParam(required = false) Integer from,
-                                                    @RequestParam(required = false) Integer to,
-                                                    @RequestParam(required = false) String filterType) {
+    public CollectionModel<BookDTO> getBooksBasedOnFilters(@RequestParam(required = false) Integer id, @RequestParam(required = false) String name,
+                                                           @RequestParam(required = false) String author,
+                                                           @RequestParam(required = false) Integer cost, @RequestParam(required = false) Integer from,
+                                                           @RequestParam(required = false) Integer to,
+                                                           @RequestParam(required = false) String filterType) {
         List<BookDTO> response = bookService.getBooksByFilters(id, name, author, cost, from, to, filterType);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        CollectionModel<BookDTO> model = CollectionModel.of(response);
+        WebMvcLinkBuilder selfLink = linkTo(methodOn(this.getClass()).getAllBooks());
+        model.add(selfLink.withSelfRel());
+        return model;
     }
 
     @GetMapping("getname/{id}")
